9f2e5ab808cb9a4d7a882e5ae01099fc64b2457a
mehm8128 <mehm8128@gmail.com>
2022-06-13 11:18:19 +0900

    make page layout

---
+++ src/components/Canvas.tsx
@@ -0,0 +1,5 @@
+const Canvas: React.FC = () => {
+  return <div></div>;
+};
+
+export default Canvas;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,24 @@
 import type { NextPage } from 'next';
+import Canvas from '../components/Canvas';
 
 const Home: NextPage = () => {
-  return <div className='text-xl text-red-500'>おいすー</div>;
+  return (
+    <div className='mx-auto w-300'>
+      <h1 className='my-8 text-center text-3xl'>I Love TikZ</h1>
+      <div className='flex'>
+        <div className='border h-160 w-320'>
+          <Canvas />
+        </div>
+      </div>
+      <div className='mt-2 text-right'>
+        <button className='rounded-lg bg-purple-400 text-white p-2'>TikZに変換！</button>
+      </div>
+      <section>
+        <h2>変換結果</h2>
+        <p className='border'>{/*変換後テキストを表示*/}</p>
+      </section>
+    </div>
+  );
 };
 
 export default Home;
