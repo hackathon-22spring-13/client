dd8b94e25472b075a6ad37c0ea171a2c3921b926
mehm8128 <mehm8128@gmail.com>
2022-06-19 12:51:57 +0900

    fix manual

--- src/components/ManualModal.tsx
+++ src/components/ManualModal.tsx
@@ -9,13 +9,14 @@ const WorkModal: React.FC = () => {
       onClick={() => setShouldShowManualModal(false)}
     >
       <div
-        className='bg-white my-auto mx-auto rounded-3xl h-2/5 shadow-lg px-12 pt-8 inset-0 w-1/3 absolute md:w-2/5'
+        className='bg-white my-auto mx-auto rounded-3xl h-1/2 shadow-lg px-12 pt-8 inset-0 w-1/3 absolute md:w-2/5'
         onClick={(e) => e.stopPropagation()}
       >
         <h2 className='text-center text-2xl'>説明書</h2>
         <ul className='list-disc mt-8 leading-relaxed'>
           <li>お絵描きモードでは自由に線を引けます。</li>
           <li>選択モードでは図形や線の選択・修正、選択したものの削除などができます。</li>
+          <li>Shiftキーを押しながらクリックで複数選択ができます。</li>
           <li>図形ツールは図形選択後、クリックした位置に図形を配置できます。</li>
           <li>直線ツールはドラッグで直線が引けます。</li>
           <li>
