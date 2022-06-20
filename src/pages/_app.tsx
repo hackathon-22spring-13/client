556924017647a4b6cf659185825fe6e3fe74d386
mehm8128 <mehm8128@gmail.com>
2022-06-20 15:27:46 +0900

    add head tag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,15 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import 'windi.css';
+import Head from 'next/head';
 import { RecoilRoot } from 'recoil';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <RecoilRoot>
+      <Head>
+        <title>I love TikZ</title>
+      </Head>
       <Component {...pageProps} />
     </RecoilRoot>
   );
